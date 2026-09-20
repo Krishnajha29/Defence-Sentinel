647975999b36ab474e6db1cb5c758d0bf993d490
SUDHANSHU KUMAR <sudhanshuuuu96@gmail.com>
2026-09-21 00:02:31 +0530

    add defense_radar_system/run_radar.bat

---
+++ defense_radar_system/run_radar.bat
@@ -0,0 +1,21 @@
+﻿@echo off
+title ESM-ASTRA // Tactical Situational Awareness & RF Command Workstation
+color 0B
+echo =======================================================================
+echo    ESM-ASTRA: ADAPTIVE ELECTRONIC SUPPORT & SITUATIONAL AWARENESS
+echo    Unified Multi-Sensor Defence Command & Control Workstation (SIH26055)
+echo =======================================================================
+echo.
+
+cd /d "%~dp0"
+
+IF NOT EXIST "node_modules" (
+    echo [INFO] First time setup: Installing server dependencies...
+    call npm.cmd install
+)
+
+echo [INFO] Starting ESM-ASTRA Workstation Server on http://localhost:8080 ...
+start "" "http://localhost:8080"
+node server.js
+
+pause
